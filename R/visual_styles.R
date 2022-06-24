2b600e5d0250ba6d8c4ecdac95017726b1897362
Robert Koetsier <robertkoetsier778@gmail.com>
2022-06-24 12:12:03 +0200

    group color selection and test complete

---
+++ R/visual_styles.R
@@ -0,0 +1,59 @@
+
+n_distinct_cols <- function(n, colors = NULL) {
+
+  # check correct n is supplied
+  if (!is.numeric(n) | length(n) > 1) {
+    stop("Must provide a single number to select colors",
+         "\nℹ class(n) for your n: ", class(n),
+         call.=FALSE)
+  }
+
+  # Check if at least one color is selected
+  if (n < 1) {
+    stop("Must select at least one color",
+    "\nℹ argument n is: ", n,
+    "\n✖ n must be 1 or greater", call.=FALSE)
+  }
+
+  # Round n and convert into integer
+  n <- as.integer(round(n, digits = 0))
+
+  # Define colors palettes with distinct colors
+  sanjee_cols4 <- as.vector(c("#0073C2", "#EFC000", "#868686", "#CD534C"))
+  c16 <- c("dodgerblue2", "#E31A1C", "green4", "#6A3D9A", "#FF7F00", "black", "gold1",
+           "skyblue2", "palegreen2", "#FDBF6F", "gray70", "maroon", "orchid1",
+           "darkturquoise", "darkorange4", "brown")
+  c25 <- c("dodgerblue2", "#E31A1C", # red
+           "green4", "#6A3D9A", # purple
+           "#FF7F00", # orange
+           "black", "gold1", "skyblue2", "#FB9A99", # lt pink
+           "palegreen2", "#CAB2D6", # lt purple
+           "#FDBF6F", # lt orange
+           "gray70", "khaki2", "maroon", "orchid1", "deeppink1", "blue1", "steelblue4",
+           "darkturquoise", "green1", "yellow4", "yellow3", "darkorange4", "brown")
+
+  # Are manual colors supplied?
+  if(is.null(colors)) {
+    # If fewer colors are returned we try to select prettier ones
+    if (n <= 4) {
+      return(sanjee_cols4[1:n])
+    } else if (n <= 16) {
+      return(c16[1:n])
+    } else if (n <= 25) {
+      return(c25[1:n])
+    } else if (n > 25) {
+      return(rep(c25, length.out = n))
+    }
+    # When manual colors are supplied return a selection of those instead
+  } else {
+
+    # Colors must be a character vector
+    if (!is.character(colors)) {
+      stop("Must provide colors as character vector",
+           "\nℹ You provided an object of class: ", class(colors),
+           call.=FALSE)
+    }
+
+    return(rep(colors, length.out = n))
+  }
+}
---
+++ tests/testthat/test-visual_styles.R
@@ -0,0 +1,26 @@
+test_that("color selection gives error for non-numeric", {
+  expect_error(n_distinct_cols("a"), "provide a single number")
+})
+
+test_that("color selection gives error for length n>1", {
+  expect_error(n_distinct_cols(c(1,2)), "provide a single number")
+})
+
+test_that("color selection works only when at least one color is requested", {
+  expect_error(n_distinct_cols(0.99), "select at least one color")
+  expect_error(n_distinct_cols(-1), "select at least one color")
+})
+
+test_that("color selection gives correct number of colors", {
+  expect_equal(n_distinct_cols(1) %>% length(), 1)
+  expect_equal(n_distinct_cols(4) %>% length(), 4)
+  expect_equal(n_distinct_cols(14) %>% length(), 14)
+  expect_equal(n_distinct_cols(24) %>% length(), 24)
+  expect_equal(n_distinct_cols(51) %>% length(), 51)
+  expect_equal(n_distinct_cols(1, c("a", "b", "c")) %>% length(), 1)
+  expect_equal(n_distinct_cols(51, c("a", "b", "c")) %>% length(), 51)
+})
+
+test_that("color selection with custom colors fails when those are not character", {
+  expect_error(n_distinct_cols(3, c(1,2,3)), "colors as character vector")
+})
