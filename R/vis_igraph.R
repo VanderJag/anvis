4244cf6c3c6f1e36cfef0bab2d055677c52ca114
Robert Koetsier <robertkoetsier778@gmail.com>
2022-06-24 12:11:34 +0200

    succes experiment to plot grouped nodes together in circular arrangement

--- R/vis_igraph.R
+++ R/vis_igraph.R
@@ -1,6 +1,7 @@
 # library(igraph)
+# library(testthat)
 #
-# Mat1 <- readRDS("tests/trail_adjacency_matrix.rds")
+# Mat1 <- readRDS(test_path("fixtures", "trail_adjacency_matrix.rds"))
 #
 # # Some grouping based on column names
 # group_vec <- rep("A", times = nrow(Mat1))
@@ -12,9 +13,21 @@
 #
 #
 # vis_in_igraph <- function(edge_table, node_table) {
-#   my_graph <- graph_from_adjacency_matrix(Mat1,
-#                                           mode = "undirected",
-#                                           weighted = TRUE,
-#                                           diag = FALSE)
-#   plot(my_graph, layout=layout_in_circle)
+#
 # }
+#
+# my_graph <- graph_from_adjacency_matrix(Mat1,
+#                                         mode = "undirected",
+#                                         weighted = TRUE,
+#                                         diag = FALSE)
+# group_layout <- layout_in_circle(my_graph, order = order(group_vec))
+# V(my_graph)$color <-
+# plot(my_graph, layout = group_layout)
+#
+#
+# lookup_table <- data.frame()
+#
+# dat <- dat%>%left_join(lookup_table)
+#
+# colnames(Mat1)[order(group_vec)]
+
