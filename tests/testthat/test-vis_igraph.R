683e113d8691ea31a693b736047209f945f068d5
Robert Koetsier <robertkoetsier778@gmail.com>
2022-07-04 13:06:38 +0200

    formalized test to visualize in igraph

--- tests/testthat/test-vis_igraph.R
+++ tests/testthat/test-vis_igraph.R
@@ -26,3 +26,27 @@ test_that("igraph_obj input must be of class igraph", {
 })
 
 
+# test_that("igraph visualization runs without error",{
+#
+#   Mat1 <- readRDS(testthat::test_path("fixtures", "trail_adjacency_matrix.rds"))
+#
+#   group_vec <- rep("A", times = nrow(Mat1))
+#   group_vec[colnames(Mat1) |> stringr::str_detect("IL")] <- "B"
+#   group_vec[colnames(Mat1) |> stringr::str_detect("CCL")] <- "C"
+#   group_vec[colnames(Mat1) |> stringr::str_detect("CXCL")] <- "D"
+#
+#   network_list <- adj_matrix_to_network(Mat1,
+#                                         group_vec = group_vec,
+#                                         width_type = 1)
+#   edge_table <- network_list[["edge_table"]]
+#   node_table <- network_list[["node_table"]]
+#
+#   expect_error(vis_igraph(edge_table, node_table, rot_labs = T,
+#                           rot_labs_opts = list(cex = 1.2),
+#                           vertex.color = c(rep("black", 18),
+#                                            rep("yellow", 18))),
+#                NA)
+# })
+
+
+
