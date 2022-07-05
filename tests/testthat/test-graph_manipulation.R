7549a0326b82f35b339300bf3ef17477591f996f
Robert Koetsier <robertkoetsier778@gmail.com>
2022-07-05 16:38:53 +0200

    several tests to check on the edge width calculation

--- tests/testthat/test-graph_manipulation.R
+++ tests/testthat/test-graph_manipulation.R
@@ -4,10 +4,96 @@ test_that("edge width works without error for all types", {
   # Create edge table
   edge_table <- adj_matrix_to_edgelist(Mat1)
 
-  expect_error(edge_weight_to_widths(edge_table = edge_table, 1), NA)
-  expect_error(edge_weight_to_widths(edge_table = edge_table, 2), NA)
-  expect_error(edge_weight_to_widths(edge_table = edge_table, 3), NA)
-  expect_error(edge_weight_to_widths(edge_table = edge_table, 4), NA)
-  expect_error(edge_weight_to_widths(edge_table = edge_table, 5), NA)
+  expect_error(edge_weight_to_widths(edge_table = edge_table, width_type = "cor"), NA)
+  expect_error(edge_weight_to_widths(edge_table = edge_table, width_type = "partcor"), NA)
+  expect_error(edge_weight_to_widths(edge_table = edge_table, width_type = "MI"), NA)
+  expect_error(edge_weight_to_widths(edge_table = edge_table, width_type = "ranked"), NA)
+  expect_error(edge_weight_to_widths(edge_table = edge_table, width_type = "percentile"), NA)
+})
+
+
+test_that("all edge width types return a column called width", {
+  # Load adjacency matrix
+  Mat1 <- readRDS(test_path("fixtures", "trail_adjacency_matrix.rds"))
+  # Create edge table
+  edge_table <- adj_matrix_to_edgelist(Mat1)
+
+  add_edge_widths <- . %>%
+    edge_weight_to_widths(width_type = ., edge_table = edge_table)
+
+  expect_true("width" %in% colnames("cor" %>% add_edge_widths))
+  expect_true("width" %in% colnames("partcor" %>% add_edge_widths))
+  expect_true("width" %in% colnames("MI" %>% add_edge_widths))
+  expect_true("width" %in% colnames("ranked" %>% add_edge_widths))
+  expect_true("width" %in% colnames("percentile" %>% add_edge_widths))
+})
+
+
+test_that("order of edge width is the same as edge weight", {
+  # Load adjacency matrix
+  Mat1 <- readRDS(test_path("fixtures", "trail_adjacency_matrix.rds"))
+  # Create edge table
+  edge_table <- adj_matrix_to_edgelist(Mat1)
+  # Check order of edges
+  ordered <- dplyr::arrange(edge_table, desc(abs(weight)))
+
+  test_prep <- . %>%
+    edge_weight_to_widths(width_type = ., edge_table = edge_table) %>%
+    dplyr::arrange(dplyr::desc(abs(width))) %>%
+    dplyr::select(-width)
+
+  expect_equal("cor" %>% test_prep, ordered)
+  expect_equal("partcor" %>% test_prep, ordered)
+  expect_equal("MI" %>% test_prep, ordered)
+  expect_equal("ranked" %>% test_prep, ordered)
+  # Percentiles have the same width so the ordering trick of this test does not
+  #   work for the "percentile" option.
+  # expect_equal("percentile" %>% test_prep, ordered)
+})
+
+
+test_that("edge width are in the expected range of 0 to 1", {
+  # Load adjacency matrix
+  Mat1 <- readRDS(test_path("fixtures", "trail_adjacency_matrix.rds"))
+  # Create edge table
+  edge_table <- adj_matrix_to_edgelist(Mat1)
+
+  get_edge_widths <- . %>%
+    edge_weight_to_widths(width_type = ., edge_table = edge_table) %>%
+    dplyr::pull(width)
+
+  expect_true(range("cor" %>% get_edge_widths)[1] >= 0)
+  expect_true(range("cor" %>% get_edge_widths)[2] <= 1)
+  expect_true(range("partcor" %>% get_edge_widths)[1] >= 0)
+  expect_true(range("partcor" %>% get_edge_widths)[2] <= 1)
+  expect_true(range("MI" %>% get_edge_widths)[1] >= 0)
+  expect_true(range("MI" %>% get_edge_widths)[2] <= 1)
+  expect_true(range("ranked" %>% get_edge_widths)[1] >= 0)
+  expect_true(range("ranked" %>% get_edge_widths)[2] <= 1)
+  expect_true(range("percentile" %>% get_edge_widths)[1] >= 0)
+  expect_true(range("percentile" %>% get_edge_widths)[2] <= 1)
+})
+
+
+test_that("order of edges gets maintained after adding widths column", {
+  # Load adjacency matrix
+  Mat1 <- readRDS(test_path("fixtures", "trail_adjacency_matrix.rds"))
+  # Create edge table
+  edge_table <- adj_matrix_to_edgelist(Mat1)
+
+  test_prep <- . %>%
+    edge_weight_to_widths(width_type = ., edge_table = edge_table) %>%
+    dplyr::arrange(dplyr::desc(abs(width))) %>%
+    dplyr::select(-width)
+  expect_equal(edge_weight_to_widths(edge_table, "cor") %>% {paste(.$source, .$target)},
+               edge_table %>% {paste(.$source, .$target)})
+  expect_equal(edge_weight_to_widths(edge_table, "partcor") %>% {paste(.$source, .$target)},
+               edge_table %>% {paste(.$source, .$target)})
+  expect_equal(edge_weight_to_widths(edge_table, "MI") %>% {paste(.$source, .$target)},
+               edge_table %>% {paste(.$source, .$target)})
+  expect_equal(edge_weight_to_widths(edge_table, "ranked") %>% {paste(.$source, .$target)},
+               edge_table %>% {paste(.$source, .$target)})
+  expect_equal(edge_weight_to_widths(edge_table, "percentile") %>% {paste(.$source, .$target)},
+               edge_table %>% {paste(.$source, .$target)})
 })
 
