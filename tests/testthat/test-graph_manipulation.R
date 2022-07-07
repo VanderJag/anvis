b7903961ad7e8e1bc85fadd871e7f22ea2e09282
Robert Koetsier <robertkoetsier778@gmail.com>
2022-07-07 16:42:24 +0200

    tests for validity of connectivity and adjusting old tests to ignore newly implemented warnings

--- tests/testthat/test-graph_manipulation.R
+++ tests/testthat/test-graph_manipulation.R
@@ -6,7 +6,8 @@ test_that("edge width works without error for all types", {
 
   expect_error(edge_weight_to_widths(edge_table = edge_table, width_type = "cor"), NA)
   expect_error(edge_weight_to_widths(edge_table = edge_table, width_type = "partcor"), NA)
-  expect_error(edge_weight_to_widths(edge_table = edge_table, width_type = "MI"), NA)
+  expect_error(edge_weight_to_widths(edge_table = edge_table, width_type = "MI") %>%
+                 suppressWarnings(), NA)
   expect_error(edge_weight_to_widths(edge_table = edge_table, width_type = "ranked"), NA)
   expect_error(edge_weight_to_widths(edge_table = edge_table, width_type = "percentile"), NA)
 })
@@ -23,7 +24,8 @@ test_that("all edge width types return a column called width", {
 
   expect_true("width" %in% colnames("cor" %>% add_edge_widths))
   expect_true("width" %in% colnames("partcor" %>% add_edge_widths))
-  expect_true("width" %in% colnames("MI" %>% add_edge_widths))
+  expect_true("width" %in% colnames("MI" %>% add_edge_widths) %>%
+                suppressWarnings())
   expect_true("width" %in% colnames("ranked" %>% add_edge_widths))
   expect_true("width" %in% colnames("percentile" %>% add_edge_widths))
 })
@@ -33,13 +35,13 @@ test_that("Warning is shown when range doesn't match partcor or cor", {
   # Create artificial edgelist with column 'weight'
   df <- tibble::tibble("weight" = seq(0,3.6, length.out = 40))
 
-  expect_warning(edge_weight_to_widths(df, "partcor"), "ideal range")
-  expect_warning(edge_weight_to_widths(df, "cor"), "ideal range")
+  expect_warning(edge_weight_to_widths(df, "partcor"), "Unexpected results may be returned")
+  expect_warning(edge_weight_to_widths(df, "cor"), "Unexpected results may be returned")
 
   df <- tibble::tibble("weight" = seq(-5, 0, length.out = 40))
 
-  expect_warning(edge_weight_to_widths(df, "partcor"), "ideal range")
-  expect_warning(edge_weight_to_widths(df, "cor"), "ideal range")
+  expect_warning(edge_weight_to_widths(df, "partcor"), "Unexpected results may be returned")
+  expect_warning(edge_weight_to_widths(df, "cor"), "Unexpected results may be returned")
 })
 
 
@@ -66,7 +68,7 @@ test_that("order of edge width is the same as edge weight", {
 
   expect_equal("cor" %>% test_prep, ordered)
   expect_equal("partcor" %>% test_prep, ordered)
-  expect_equal("MI" %>% test_prep, ordered)
+  expect_equal(suppressWarnings("MI" %>% test_prep), ordered)
   expect_equal("ranked" %>% test_prep, ordered)
   # Percentiles have the same width so the ordering trick of this test does not
   #   work for the "percentile" option.
@@ -88,8 +90,8 @@ test_that("edge width are in the expected range of 0 to 1", {
   expect_true(range("cor" %>% get_edge_widths)[2] <= 1)
   expect_true(range("partcor" %>% get_edge_widths)[1] >= 0)
   expect_true(range("partcor" %>% get_edge_widths)[2] <= 1)
-  expect_true(range("MI" %>% get_edge_widths)[1] >= 0)
-  expect_true(range("MI" %>% get_edge_widths)[2] <= 1)
+  expect_true(suppressWarnings(range("MI" %>% get_edge_widths)[1]) >= 0)
+  expect_true(suppressWarnings(range("MI" %>% get_edge_widths)[2]) <= 1)
   expect_true(range("ranked" %>% get_edge_widths)[1] >= 0)
   expect_true(range("ranked" %>% get_edge_widths)[2] <= 1)
   expect_true(range("percentile" %>% get_edge_widths)[1] >= 0)
@@ -111,7 +113,7 @@ test_that("order of edges gets maintained after adding widths column", {
                edge_table %>% {paste(.$source, .$target)})
   expect_equal(edge_weight_to_widths(edge_table, "partcor") %>% {paste(.$source, .$target)},
                edge_table %>% {paste(.$source, .$target)})
-  expect_equal(edge_weight_to_widths(edge_table, "MI") %>% {paste(.$source, .$target)},
+  expect_equal(suppressWarnings(edge_weight_to_widths(edge_table, "MI")) %>% {paste(.$source, .$target)},
                edge_table %>% {paste(.$source, .$target)})
   expect_equal(edge_weight_to_widths(edge_table, "ranked") %>% {paste(.$source, .$target)},
                edge_table %>% {paste(.$source, .$target)})
@@ -119,3 +121,41 @@ test_that("order of edges gets maintained after adding widths column", {
                edge_table %>% {paste(.$source, .$target)})
 })
 
+
+test_that("connectivity throws error when adj_matrix and node_table have different nodes", {
+  # Load adjacency matrix
+  Mat1 <- readRDS(testthat::test_path("fixtures", "trail_adjacency_matrix.rds"))
+
+  network_list <- adj_matrix_to_network(Mat1,
+                                        group_vec = group_vec,
+                                        width_type = "partcor")
+  node_table <- network_list[["node_table"]]
+
+  # Remove elements
+  Mat1 <- Mat1[,-2]
+  node_table <- node_table %>% dplyr::filter(node != "ESelectin")
+
+  expect_error(node_size_connectivity(node_table = node_table, Mat1, vis_type = NULL),
+               "Must provide node_table and adj_matrix with the same nodes")
+})
+
+
+test_that("connectivity rowSums matches one determined based on edge weights", {
+  # Load adjacency matrix
+  Mat1 <- readRDS(testthat::test_path("fixtures", "trail_adjacency_matrix.rds"))
+
+  network_list <- adj_matrix_to_network(Mat1,
+                                        group_vec = group_vec,
+                                        width_type = "partcor")
+  edge_table <- network_list[["edge_table"]]
+  node_table <- network_list[["node_table"]]
+  node_table <- node_size_connectivity(node_table = node_table, Mat1, vis_type = NULL)
+
+  # Calculate by alternative means
+  conns2 <- sapply(node_table$node,
+                   function (x) sum(abs(c(edge_table[edge_table$source == x,]$weight,
+                                           edge_table[edge_table$target == x,]$weight))))
+  conns2 <- (conns2 / max(conns2)) %>% sigmoid_xB(3) %>% unname()
+
+  expect_equal(node_table$size, conns2)
+})
