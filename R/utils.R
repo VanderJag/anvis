9644b518afb695c782d790af1f1e6d6f055dea10
Robert Koetsier <robertkoetsier778@gmail.com>
2022-07-13 17:23:24 +0200

    check for file name with two extensions at once

--- R/utils.R
+++ R/utils.R
@@ -7,3 +7,19 @@ file_sequence <- function(name_base, ext) {
     i = i + 1
   }
 }
+
+file_pair_seq <- function(name_base, ext1, ext2) {
+  if (!file.exists(paste0(name_base, ext1)) &&
+      !file.exists(paste0(name_base, ext2))) return(name_base)
+  i = 2
+  repeat {
+    seq_next = paste0(name_base, "_", i)
+    if (!file.exists(paste0(seq_next, ext1)) &&
+        !file.exists(paste0(seq_next, ext2))) return(seq_next)
+    i = i + 1
+  }
+}
+
+# Defaults for NULL values
+`%||%` <- function(a, b) if (is.null(a)) b else a
+
