5a1fc41e07918fe33a92dc1f90447d994b316ad7
Robert Koetsier <robertkoetsier778@gmail.com>
2022-07-25 14:13:35 +0200

    check whether radial labels work when nodes are arranged by connectivity

--- tests/testthat/test-vis_cytoscape.R
+++ tests/testthat/test-vis_cytoscape.R
@@ -422,3 +422,32 @@ test_that("no error when node or edge attributes are missing", {
                                 save_session = FALSE, export_image = FALSE),
                NA)
 })
+
+
+test_that("radial labels work when nodes are arranged by connectivity",{
+  Mat1 <- readRDS(testthat::test_path("fixtures", "trail_adjacency_matrix.rds"))
+  group_vec <- readRDS(test_path("fixtures", "group_vec_adj_matrix.rds"))
+
+  network_list <- adj_matrix_to_network(Mat1,
+                                        node_attrs = "all",
+                                        edge_attrs = "all",
+                                        group_vec = group_vec,
+                                        width_type = "partcor",
+                                        size_type = "cytoscape")
+  edge_table <- network_list[["edge_table"]]
+  node_table <- network_list[["node_table"]]
+  node_table <- sort_avg_connectivity(node_table)[[1]]
+
+  test_call <- deparse(sys.calls()[[1]][1])
+  skip_if_not(test_call == "test_that()",
+              message = "cytoscape visualizations need to be checked manually")
+
+  # Test for presence of cytoscape
+  cytosc <- RCy3::cytoscapePing() %>% capture_condition()
+  skip_if_not(cytosc$message == "You are connected to Cytoscape!\n",
+              message = "this test runs only when cytoscape is active")
+
+  expect_error(vis_in_cytoscape(edge_table = edge_table, node_table = node_table,
+                                save_session = FALSE),
+               NA)
+})
