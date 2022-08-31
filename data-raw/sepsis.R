763f6180f7fb1cf5a604e32e75c4776f31635913
Robert Koetsier <robertkoetsier778@gmail.com>
2022-08-31 15:44:20 +0200

    renaming dataset to sepsis

--- data-raw/sepsis.R
+++ data-raw/sepsis.R
@@ -77,7 +77,7 @@ PCLRC.gmm <- function(datamatrix, prob.threshold = 0.95, Niter=1000, frac=0.75, 
 
 
 
-    CorrMat = ggm.estimate.pcor(as.matrix(datamatrix), method = "static", verbose = F)
+    CorrMat = GeneNet::ggm.estimate.pcor(as.matrix(datamatrix), method = "static", verbose = F)
     CorrMat = as.matrix(unclass(CorrMat))
 
     CorrMatFiltered  <- CorrMat
@@ -93,18 +93,20 @@ datamatrix <- readr::read_tsv("data-raw/Set01_imputed.txt") %>%
     dplyr::select(-c(1:12), -c("IL-1ra", "IL-23_ELISA", "IL_33_ELISA"))
 network_for_visualisation <- PCLRC.gmm(datamatrix, prob.threshold = 0.95, Niter=1000, frac=0.75, rank.thr=0.3)
 
+sepsis <- network_for_visualisation$CorrMatFiltered
+
 # Visualization to compare with the other dataset
 
-groups <- rep("A", nrow(network_for_visualisation$CorrMatFiltered))
-idx_ccl <- which(str_detect(colnames(network_for_visualisation$CorrMatFiltered), "^CCL"))
-idx_il <- which(str_detect(colnames(network_for_visualisation$CorrMatFiltered), "^IL"))
-idx_cxcl <- which(str_detect(colnames(network_for_visualisation$CorrMatFiltered), "^CXCL"))
+groups <- rep("A", nrow(sepsis))
+idx_ccl <- which(stringr::str_detect(colnames(sepsis), "^CCL"))
+idx_il <- which(stringr::str_detect(colnames(sepsis), "^IL"))
+idx_cxcl <- which(stringr::str_detect(colnames(sepsis), "^CXCL"))
 groups[idx_il] <- "B"
 groups[idx_ccl] <- "C"
 groups[idx_cxcl] <- "D"
 
-network_for_visualisation$CorrMatFiltered %>% VisualiseNetwork(node_attrs = "all", edge_attrs = "all", group_vec = groups)
+sepsis %>% VisualiseNetwork(node_attrs = "all", edge_attrs = "all", group_vec = groups)
 
-STI <- network_for_visualisation$CorrMatFiltered
+sepsis <- sepsis
 
-usethis::use_data(STI, overwrite = TRUE)
+usethis::use_data(sepsis, overwrite = TRUE)
