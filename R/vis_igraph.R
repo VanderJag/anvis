1887126aaa3c13bdea85a0c4b0dde8bfb6bed174
Robert Koetsier <robertkoetsier778@gmail.com>
2022-09-07 10:33:05 +0200

    added compatibility of different network types, igraph, graphNEL and list of dataframes can now be used as input

--- R/vis_igraph.R
+++ R/vis_igraph.R
@@ -71,7 +71,7 @@ visIgraph <- function(network,
                           igraph::is_directed(network)
                       } else {
                           FALSE
-                      }
+                      },
                       radial_labs = TRUE,
                       rad_lab_opts = list(),
                       scale_width = 3.25,
@@ -110,188 +110,157 @@ visIgraph <- function(network,
              call.=FALSE)
     }
 
-    # Validate network parameters ---------------------------------------------
+
+    # Check type of network ---------------------------------------------------
 
     if (is(network, "graphNEL")) {
-        network <- dfs_from_graphNEL(gr_nel = network)
-        network_type <- "graphNEL"
+        graph <- igraph::graph_from_graphnel(network)
 
     } else if (is(network, "igraph")) {
-        network <- dfs_from_igraph(igraph_obj = network)
-        network_type <- "igraph"
+        graph <- network
 
-    } else if (!all(c("vertices", "edges") %in% names(network))) {
+    } else if (is_network_list(network)) {
+        graph <- igraph::graph_from_data_frame(network$edges,
+                                               vertices = network$vertices,
+                                               directed = directed)
+    } else {
         stop("Input network must be graphNEL, igraph, or list containing data ",
              "frames named 'vertices' and 'edges'. \nℹ Class of your network: ",
              class(network), call.=FALSE)
+    }
+
+
+    # Capture input -----------------------------------------------------------
+
+    # Store additional user arguments as list
+    plot_params <- list(...)
+
+    # Get the values from additional user input or otherwise use the defaults
+    edge_width <- plot_params[["edge.width"]] %||%
+        igraph::edge.attributes(graph)[["width"]]
+    edge_color <- plot_params[["edge.color"]] %||%
+        igraph::edge.attributes(graph)[["color"]]
+    edge_arrowsize <- plot_params[["edge.arrow.size"]] %||% 0.3
+    edge_curve <- plot_params[["edge.curved"]] %||% 0
+    if (directed)   edge_curve <- plot_params[["edge.curved"]] %||% 0.05
+    vertex_color <- plot_params[["vertex.color"]] %||%
+        igraph::vertex.attributes(graph)[["color"]]
+    vertex_size <- plot_params[["vertex.size"]] %||%
+        igraph::vertex.attributes(graph)[["size"]]
+    vertex_brd_col<- plot_params[["vertex.frame.color"]] %||% "white"
+    vertex_label0 <- plot_params[["vertex.label"]] %||%
+        igraph::vertex.attributes(graph)[["name"]]
+    # If group info is present order based on group, otherwise keep node order
+    if ("group" %in% igraph::vertex_attr_names(graph)) {
+        layout_ord <- order(igraph::V(graph)$group)
     } else {
-        network_type <- "lists"
+        layout_ord <- igraph::V(graph)
     }
 
+    node_arrangement <- plot_params[["layout"]] %||%
+        igraph::layout_in_circle(graph, order = layout_ord)
 
-    # error for when all network input is missing
-    if (is.null(edge_table) & is.null(node_table) & is.null(igraph_obj)) {
-        stop("Must provide either edge and node table or igraph object",
-             "\nℹ edge_table, node_table and igraph_obj parameters are NULL", call.=FALSE)
+    # If vertex labels are to be placed radially, there should be none placed
+    #   by plot.igraph.
+    if (radial_labs) vertex_label <- "" else vertex_label <- vertex_label0
+
+
+    # Visualize in igraph -----------------------------------------------------
+
+    # Select a graphics device to save output
+    start_saving(export_type, export_opts, save_name)
+    # If an error occurs stop the opened graphical device
+    if (export_type != "print") {
+        dev_name <- if (export_type == "ps") "postscript" else export_type
+        on.exit(if (names(dev.cur()) == dev_name) dev.off())
     }
 
-  # Error when network table input is incomplete
-  if (!is.null(edge_table) & is.null(node_table) |
-      is.null(edge_table) & !is.null(node_table)) {
-    stop("Must provide both edge and node table",
-         "\nℹ ", ifelse(is.null(edge_table),
-                        "edge table parameter is NULL",
-                        "node table parameter is NULL"),
-         call.=FALSE)
-  }
+    # Change graphical parameters here, so they will affect the newly active device
+    if (length(par_opts) != 0) old_par <- do.call(par, par_opts)
 
-  # Prepare igraph graph for visualization
-  if (!is.null(edge_table) & !is.null(node_table)) {
-      # Check if the required columns are present
+    # Visualize the basic graph
+    do.call(igraph::plot.igraph,
+            c(list(x = graph,
+                   layout = node_arrangement,
+                   edge.width = edge_width * scale_width,
+                   edge.color = edge_color,
+                   edge.arrow.size = edge_arrowsize,
+                   edge.curved = edge_curve,
+                   vertex.size = vertex_size,
+                   vertex.color = vertex_color,
+                   vertex.label = vertex_label,
+                   vertex.frame.color = vertex_brd_col),
+              plot_params
+            )
+    )
 
-    graph <- igraph::graph_from_data_frame(edge_table,
-                                           vertices = node_table,
-                                           directed = directed)
-  } else {
-    if (!igraph::is_igraph(igraph_obj)) {
-      stop("`igraph_obj` parameter must be of class igraph:",
-      "\nℹ Your input is class: ", class(igraph_obj),
-      ".\n✖ igraph::is_igraph(igraph_obj) must return TRUE.", call.=FALSE)
+    # If vertex labels are to be added radially that will be done below
+    if (radial_labs) {
+        x = node_arrangement[,1]
+        y = node_arrangement[,2]
+
+        # Check if user wants to overwrite one of the default arguments
+        txt_x <- if ("x" %in% names(rad_lab_opts)) rad_lab_opts[["x"]] else x * 1.1
+        txt_y <- if ("y" %in% names(rad_lab_opts)) rad_lab_opts[["y"]] else y * 1.1
+        txt_labels <- if ("labels" %in% names(rad_lab_opts)) rad_lab_opts[["labels"]] else vertex_label0
+        txt_adj <- if ("adj" %in% names(rad_lab_opts)) rad_lab_opts[["adj"]] else ifelse(x<=0, 1, 0)
+        txt_cex <- if ("cex" %in% names(rad_lab_opts)) rad_lab_opts[["cex"]] else 0.85
+        # Remove the used arguments from list as to pass them twice, prevents error
+        rad_lab_opts[c("x","y","labels","adj","cex")] <- NULL
+
+        angle = radial_angle(x, y)
+
+        # Create a text function that vectorizes srt argument
+        text <- function(...) Map(graphics::text, ...)
+
+        do.call(text,
+                c(list(x = txt_x,
+                       y = txt_y,
+                       labels = txt_labels,
+                       adj = txt_adj,
+                       cex = txt_cex,
+                       srt = angle,
+                       xpd = T),
+                  rad_lab_opts))
     }
-    graph <- igraph_obj
-  }
 
-  # Capture input -----------------------------------------------------------
+    # reset graphical parameters to original option
+    if (length(par_opts) != 0) par(old_par)
 
-  # Store additional user arguments as list
-  plot_params <- list(...)
+    # Finish saving
+    if (export_type != "print") {
+        dev.off()
+    }
 
-  # Get the values from additional user input or otherwise use the defaults
-  edge_width <- plot_params[["edge.width"]] %||%
-      igraph::edge.attributes(graph)[["width"]]
-  edge_color <- plot_params[["edge.color"]] %||%
-      igraph::edge.attributes(graph)[["color"]]
-  edge_arrowsize <- plot_params[["edge.arrow.size"]] %||% 0.3
-  edge_curve <- plot_params[["edge.curved"]] %||% 0
-  if (directed)   edge_curve <- plot_params[["edge.curved"]] %||% 0.05
-  vertex_color <- plot_params[["vertex.color"]] %||%
-      igraph::vertex.attributes(graph)[["color"]]
-  vertex_size <- plot_params[["vertex.size"]] %||%
-      igraph::vertex.attributes(graph)[["size"]]
-  vertex_brd_col<- plot_params[["vertex.frame.color"]] %||% "white"
-  vertex_label0 <- plot_params[["vertex.label"]] %||%
-      igraph::vertex.attributes(graph)[["name"]]
-  # If group info is present order based on group, otherwise keep node order
-  if ("group" %in% igraph::vertex_attr_names(graph)) {
-      layout_ord <- order(igraph::V(graph)$group)
-  } else {
-      layout_ord <- igraph::V(graph)
-  }
-
-  node_arrangement <- plot_params[["layout"]] %||%
-      igraph::layout_in_circle(graph, order = layout_ord)
-
-  # If vertex labels are to be placed radially, there should be none placed
-  #   by plot.igraph.
-  if (radial_labs) vertex_label <- "" else vertex_label <- vertex_label0
-
-
-  # Visualize in igraph -----------------------------------------------------
-
-  # Select a graphics device to save output
-  start_saving(export_type, export_opts, save_name)
-  # If an error occurs stop the opened graphical device
-  if (export_type != "print") {
-      dev_name <- if (export_type == "ps") "postscript" else export_type
-      on.exit(if (names(dev.cur()) == dev_name) dev.off())
-  }
-
-  # Change graphical parameters here, so they will affect the newly active device
-  if (length(par_opts) != 0) old_par <- do.call(par, par_opts)
-
-  # Visualize the basic graph
-  do.call(igraph::plot.igraph,
-          c(list(x = graph,
-            layout = node_arrangement,
-            edge.width = edge_width * scale_width,
-            edge.color = edge_color,
-            edge.arrow.size = edge_arrowsize,
-            edge.curved = edge_curve,
-            vertex.size = vertex_size,
-            vertex.color = vertex_color,
-            vertex.label = vertex_label,
-            vertex.frame.color = vertex_brd_col),
-            plot_params
-          )
-  )
-
-  # If vertex labels are to be added radially that will be done below
-  if (radial_labs) {
-    x = node_arrangement[,1]
-    y = node_arrangement[,2]
-
-    # Check if user wants to overwrite one of the default arguments
-    txt_x <- if ("x" %in% names(rad_lab_opts)) rad_lab_opts[["x"]] else x * 1.1
-    txt_y <- if ("y" %in% names(rad_lab_opts)) rad_lab_opts[["y"]] else y * 1.1
-    txt_labels <- if ("labels" %in% names(rad_lab_opts)) rad_lab_opts[["labels"]] else vertex_label0
-    txt_adj <- if ("adj" %in% names(rad_lab_opts)) rad_lab_opts[["adj"]] else ifelse(x<=0, 1, 0)
-    txt_cex <- if ("cex" %in% names(rad_lab_opts)) rad_lab_opts[["cex"]] else 0.85
-    # Remove the used arguments from list as to pass them twice, prevents error
-    rad_lab_opts[c("x","y","labels","adj","cex")] <- NULL
-
-    angle = radial_angle(x, y)
-
-    # Create a text function that vectorizes srt argument
-    text <- function(...) Map(graphics::text, ...)
-
-    do.call(text,
-            c(list(x = txt_x,
-                   y = txt_y,
-                   labels = txt_labels,
-                   adj = txt_adj,
-                   cex = txt_cex,
-                   srt = angle,
-                   xpd = T),
-              rad_lab_opts))
-  }
-
-  # reset graphical parameters to original option
-  if (length(par_opts) != 0) par(old_par)
-
-  # Finish saving
-  if (export_type != "print") {
-    dev.off()
-  }
-
-  invisible(NULL)
+    invisible(NULL)
 }
 
 # Internal function that starts graphical devices for saving.
 start_saving <- function(export_type, export_opts, save_name) {
-  if (export_type != "print") {
-    save_funcs <- list("png" = png, "pdf" = pdf, "svg" = svg, "jpeg" = jpeg,
-                       "tiff" = tiff, "bmp" = bmp, "ps" = postscript)
-    save_dev <- save_funcs[[export_type]]
+    if (export_type != "print") {
+        save_funcs <- list("png" = png, "pdf" = pdf, "svg" = svg, "jpeg" = jpeg,
+                           "tiff" = tiff, "bmp" = bmp, "ps" = postscript)
+        save_dev <- save_funcs[[export_type]]
 
-    # The default ('png') creates very low resolution images, fix this
-    if (export_type == "png") {
-      export_opts[["res"]] <- export_opts[["res"]] %||% 300
-      export_opts[["width"]] <- export_opts[["width"]] %||% 2400
-      export_opts[["height"]] <- export_opts[["height"]] %||% 2400
+        # The default ('png') creates very low resolution images, fix this
+        if (export_type == "png") {
+            export_opts[["res"]] <- export_opts[["res"]] %||% 300
+            export_opts[["width"]] <- export_opts[["width"]] %||% 2400
+            export_opts[["height"]] <- export_opts[["height"]] %||% 2400
+        }
+
+        # Set the save name
+        save_name <- file_sequence(save_name, paste0(".", export_type))
+        save_name <- paste0(save_name, ".", export_type)
+        if (export_type %in% c("bmp", "jpeg", "tiff", "png", "svg")) {
+            export_opts[["filename"]] <- save_name
+        } else if (export_type %in% c("pdf", "ps")) {
+            export_opts[["file"]] <- save_name
+        }
+
+        # Start graphics device
+        do.call(save_dev,
+                export_opts)
     }
-
-    # Set the save name
-    save_name <- file_sequence(save_name, paste0(".", export_type))
-    save_name <- paste0(save_name, ".", export_type)
-    if (export_type %in% c("bmp", "jpeg", "tiff", "png", "svg")) {
-      export_opts[["filename"]] <- save_name
-    } else if (export_type %in% c("pdf", "ps")) {
-      export_opts[["file"]] <- save_name
-    }
-
-    # Start graphics device
-    do.call(save_dev,
-            export_opts)
-  }
-  invisible(NULL)
+    invisible(NULL)
 }
