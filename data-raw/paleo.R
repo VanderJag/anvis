85be7ddb82072c882260fc04a32f3385164f2364
Robert Koetsier <robertkoetsier778@gmail.com>
2022-09-27 15:55:58 +0200

    updated data prep

--- data-raw/paleo.R
+++ data-raw/paleo.R
@@ -1,40 +1,36 @@
+# -------------------------------------------------------------------------
+# When using the PCLRC algorithm please cite the references:
+# DOI: 10.1021/acs.jproteome.5b00344
+# DOI: 10.1021/acs.jproteome.8b00781
+
+# Find more info and a download for PCLRC on the following page:
+# https://www.systemsbiology.nl/download-page/
+# -------------------------------------------------------------------------
+
 # Load functions to create adjacency matrix
 source("data-raw/relational_networks.R")
 
+library(magrittr)       # for %>% operator
+
+
 # Read a file from doi:10.1016/j.margeo.2005.02.009
 #   on protein levels for patients with sepsis
-end_comment <- readLines("data-raw/Elemental_dsdp_48_401_thomas_etal2005.tab") %>%
+end_comment <- readLines("data-raw/paleo_data_Elemental_dsdp_48_401_thomas_etal2005.tab") %>%
     stringr::str_detect("^\\*/") %>% which()
-datamatrix <- readr::read_tsv("data-raw/Elemental_dsdp_48_401_thomas_etal2005.tab",
+datamatrix <- readr::read_tsv("data-raw/paleo_data_Elemental_dsdp_48_401_thomas_etal2005.tab",
                               skip = end_comment) %>%
     dplyr::select(-1)
 
+
+
 # Get partial correlations and probability matrix
-network_for_visualisation <- PCLRC.gmm(datamatrix,
-                                       prob.threshold = 0.95,
-                                       Niter=1000,
-                                       frac=0.75,
-                                       rank.thr=0.3)
+network_for_visualisation <- PCLRC.mi(datamatrix)
 
 # Save just partial correlation values
-paleo <- network_for_visualisation$CorrMatFiltered
+paleo <- network_for_visualisation$MiMatFiltered
 
 # Remove additional attributes
 rm_attrs <- attributes(paleo) %>% names() %>% {.[!. %in% c("dim", "dimnames")]}
 for (at in rm_attrs) attr(paleo, at) <- NULL
-
-# Visualization to compare with the other dataset
-## Create group labels
-# idx_ccl <- which(stringr::str_detect(colnames(sepsis), "^CCL"))
-# idx_il <- which(stringr::str_detect(colnames(sepsis), "^IL"))
-# idx_cxcl <- which(stringr::str_detect(colnames(sepsis), "^CXCL"))
-# groups <- rep("A", nrow(sepsis))
-# groups[idx_il] <- "B"
-# groups[idx_ccl] <- "C"
-# groups[idx_cxcl] <- "D"
-# ## Visualize
-# sepsis %>% VisualiseNetwork(node_attrs = "all",
-#                             edge_attrs = "all",
-#                             group_vec = groups)
 
 usethis::use_data(paleo, overwrite = TRUE)
--- data-raw/relational_networks.R
+++ data-raw/relational_networks.R
@@ -1,7 +1,17 @@
+# -------------------------------------------------------------------------
+# When using the PCLRC algorithm please cite the references:
+# DOI: 10.1021/acs.jproteome.5b00344
+# DOI: 10.1021/acs.jproteome.8b00781
+
+# Find more info and a download for PCLRC on the following page:
+# https://www.systemsbiology.nl/download-page/
+# -------------------------------------------------------------------------
+
 # This file holds the main functions that are used to create example networks
 
 # library(GeneNet)
 # library(minet)
+# library(infotheo)
 
 getHighestLinks <- function(matrix, rank.thr=0.3, verbose=FALSE)    {
 
@@ -27,9 +37,7 @@ getHighestLinks <- function(matrix, rank.thr=0.3, verbose=FALSE)    {
     return(net)
 }
 
-# When using the PCLRC algorithm please cite the references:
-# DOI: 10.1021/acs.jproteome.5b00344
-# DOI: 10.1021/acs.jproteome.8b00781
+
 PCLRC.gmm <- function(datamatrix, prob.threshold = 0.95, Niter=1000, frac=0.75, rank.thr=0.3){
 
     # Check if input parameters are in correct range
@@ -84,6 +92,68 @@ PCLRC.gmm <- function(datamatrix, prob.threshold = 0.95, Niter=1000, frac=0.75, 
     diag(CorrMatFiltered) = 1
 
     networks= list(CorrMatFiltered=CorrMatFiltered,CorrMat=CorrMat,ProbMat=ProbMat,prob.threshold = prob.threshold)
+
+    return(networks)
+}
+
+
+PCLRC.mi <- function(datamatrix, mi.estimator = 'mi.empirical', discretization = 'equalwidth', prob.threshold = 0.95, Niter=1000, frac=0.75, rank.thr=0.3){
+
+    if(prob.threshold > 1 || prob.threshold < 0)
+        stop('prob.threshold must be between 0 and 1')
+
+    if(frac > 1 || frac < 0)
+        stop('frac must be between 0 and 1. Default 0.75')
+
+    if(rank.thr  > 1 || rank.thr  < 0)
+        stop('frac must be between 0 and 1. Default 0.35')
+
+    nsamp=round(dim(datamatrix)[1]*frac) #number of samples per iteration
+    Nvaliter=0  #number of valid iterations (no NAs generated)
+
+    #this table will store the number of times an interaction was selected
+    table <- mat.or.vec(dim(datamatrix)[2], dim(datamatrix)[2])
+
+
+    for( it in (1:Niter)){
+
+        #randomly select the samples
+        samples=sample(dim(datamatrix)[1], nsamp)
+
+        #build similarity matrix based on mutual information
+
+        #CONNECTIVITY based on MI
+        similarityMatrixSubset = minet::build.mim(as.data.frame(datamatrix[samples,]),
+                                                  estimator = mi.estimator,
+                                                  disc = discretization)
+        similarityMatrixSubset <- similarityMatrixSubset^2
+
+        #infeer network for this iteration using CLR
+
+        adjSubset <- minet::clr(similarityMatrixSubset)
+
+        if(!is.na(sum(adjSubset))){ #valid iteration
+            #extract highest links
+            out=getHighestLinks(adjSubset, rank.thr)
+            #collect output
+            table <- table+out
+            Nvaliter=Nvaliter+1
+        }
+    }
+
+    ProbMat  <- table/Nvaliter
+    MI.mat = minet::build.mim(as.data.frame(datamatrix),
+                              estimator = mi.estimator,
+                              disc = discretization)
+    MI.mat.pclrc  <- MI.mat
+    MI.mat.pclrc[which(ProbMat<prob.threshold)] = 0
+
+    networks= list(MiMatFiltered=MI.mat.pclrc,
+                   MiMat=MI.mat,
+                   ProbMat=ProbMat,
+                   estimator = mi.estimator,
+                   disc = discretization,
+                   prob.threshold = prob.threshold)
 
     return(networks)
 }
